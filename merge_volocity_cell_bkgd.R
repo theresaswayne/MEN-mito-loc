0618848bb2153de39958637c54bf6e065fd3ac1e
theresaswayne <tcs6@columbia.edu>
2019-02-13 16:43:43 -0500

    Finished merge_volocity_cell_bkgd
    Prompts user to select folders, includes folder name in saved data

--- merge_volocity_cell_bkgd.R
+++ merge_volocity_cell_bkgd.R
@@ -4,78 +4,78 @@
 
 # REQUIREMENTS: 
 # -- All cell data files must be within a single folder in the "data" folder in the project home
-# -- The background file must be outside this folder and must have 2 columns, ItemName and ExtracellularBackground
+# -- The background file must be one level above the cell data folder 
+# -- Background file must have 2 columns: 
+#     ItemName
+#     ExtracellularBackground
 
 # adapted from http://serialmentor.com/blog/2016/6/13/reading-and-combining-many-tidy-data-files-in-R
 
 # Setup -------
 require(here)
 require(tidyverse)
+require(tcltk)
 
-# TODO: update to allow user to select locations
+# Get background data -----
 
+bkgdfile <- tk_choose.files(default = "", caption = "Choose the CSV with background values", multi = FALSE) # file chooser window, with a message
 
-# ENTER BACKGROUND FILE INFO HERE -----
-subfolder_bkgd <- file.path("testdata", "GTY027")
-inputFolder_bkgd <- here(file.path("data",subfolder_bkgd))
-
-bkgdfile <- file.choose() # opens a file chooser window, no message
-
-xcell_bg <- read_csv(file.path(inputFolder_bkgd, bkgdfile),
+xcell_bg <- read_csv(bkgdfile,
                      locale = locale(encoding = "latin1"),
                      col_types = cols_only(
                        ItemName = "c", 
                        ExtracellularBackground = "d")) %>%
-  select(ItemName, ExtracellularBackground) # omit crazy other column(s)
+  select(ItemName, ExtracellularBackground)
 
   # can safely ignore warning message about missing column names
 
-# TODO: omit rows with NAs
-
 names(xcell_bg)[names(xcell_bg)=="ItemName"] <- "Item Name" # change column name to match the Volocity file column
 
-# ENTER CELL FOLDER INFO HERE ----------
+# Get cell data ----
 
-subfolder_cells <- file.path("testdata", "GTY027", "cells")
 
-# Read all the files in the folder ------
+# Note -- you must OPEN the folder!
 
-inputFolder_cells <- here(file.path("data",subfolder_cells))
+inputFolder_cells <- tk_choose.dir(default = "", caption = "OPEN the folder with cell data files") # prompt user
 
 # get file names
 files <- dir(inputFolder_cells, pattern = "*.csv") 
 
-# TODO: change to default all columns to double
+# read into a tibble 
+# (data_frame is deprecated)
 
-# tibble is used because of the warning that data_frame is deprecated.
-mergedDataWithNames <- tibble(filename = files) %>% # tibble holding file names
+mergedDataWithNames <- tibble(filename = files) %>% # column 1 = file names
   mutate(file_contents =
-           map(filename,          # read files into a new data column
+           map(filename,          # column 2 = data
                ~ read_csv(file.path(inputFolder_cells, .),
                           locale = locale(encoding = "latin1"),
                           na = c("", "N/A"),
                           col_types = cols(`Number of contained Mito` = col_double(),
                                            `Compartment ROIs ID` = col_double()))))
 
-# unnest to make the list into a flat file again,
-# but it now has 1 extra column to hold the filename
+# TODO: default all columns to double? (then have to specify any non-double columns)
+
+
+# make the list into a flat file
 mergedDataFlat <- unnest(mergedDataWithNames)
 
 # Simplify -------
 
-df <- filter(mergedDataFlat, Population != "Whole cells prelim") %>% # unwanted objects
-  select(-contains("Trans")) %>% # unwanted channels
+df <- filter(mergedDataFlat, Population != "Whole cells prelim") %>%  # remove unwanted objects
+  select(-contains("Trans")) %>% # remove unwanted channels
   arrange(filename,ID)  # sort rows
 
+# Merge with background data ----
 
-# TODO: Modify this to left-join the background into the merged file
-df_bkd <- df %>% left_join(xcell_bg) # only common column is ItemName
+df <- df %>% left_join(xcell_bg) # only common column is ItemName
 
 
-# Write an output file of all the merged data ----------
-outputFolder <- inputFolder_bkgd 
+# Save merged data one level up ----------
 
-outputFile = paste(Sys.Date(), "merged.csv") # spaces will be inserted
-write_csv(df_bkd,file.path(outputFolder, outputFile))
-#TODO: read background here and merge it all at once
+parentName <- basename(dirname(bkgdfile)) # parent directory without higher levels
+
+parentDir <- dirname(bkgdfile)
+
+outputFile = paste(Sys.Date(), parentName, "merged.csv") # spaces will be inserted
+write_csv(df,file.path(parentDir, outputFile))
 
